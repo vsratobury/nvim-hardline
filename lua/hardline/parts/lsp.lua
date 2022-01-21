82202a6d22ec363cc42bd79c05d2f20ae44df3ac
Всратослав Бурый <3831206+vsratobury@users.noreply.github.com>
2022-01-21 18:06:01 +0300

    remove deprecated vim.lsp.diagnostic.get_cout()

--- lua/hardline/parts/lsp.lua
+++ lua/hardline/parts/lsp.lua
@@ -1,23 +1,36 @@
-local lsp = vim.lsp
+-- local lsp = vim.lsp
 local fmt = string.format
 
 local function get_diagnostic(prefix, severity)
-  local count = lsp.diagnostic.get_count(0, severity)
-  if count < 1 then
-    return ''
-  end
-  return fmt('%s:%d', prefix, count)
+    local diagnostics = vim.diagnostic.get(0)
+    local count = {0, 0, 0, 0}
+
+    for _, diagnostic in ipairs(diagnostics) do
+        count[diagnostic.severity] = count[diagnostic.severity] + 1
+    end
+
+    local c =0;
+    if severity == 'Error' then
+        c = count[vim.diagnostic.severity.ERROR];
+    else
+        c = count[vim.diagnostic.severity.WARN];
+    end
+
+    if c < 1 then
+        return ''
+    end
+    return fmt('%s:%d', prefix, c)
 end
 
 local function get_error()
-  return get_diagnostic('E', 'Error')
+    return get_diagnostic('E', 'Error')
 end
 
 local function get_warning()
-  return get_diagnostic('W', 'Warning')
+    return get_diagnostic('W', 'Warning')
 end
 
 return {
-  get_error = get_error,
-  get_warning = get_warning,
+    get_error = get_error,
+    get_warning = get_warning,
 }
